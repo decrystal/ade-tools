a0144ca29ddf0dc400788fb8ea990d38535761cf
dechen lin <lindechen@pjlab.org.cn>
2024-03-16 22:26:13 +0800

    feat: 1

--- .dumirc.ts
+++ .dumirc.ts
@@ -86,6 +86,7 @@ export default defineConfig({
     defaultSizes: 'parsed', // stat  // gzip
 
   },
-  base: '/nicecoders.github.io',
+  base: 'nicecoders.github.io',
+  publicPath: 'nicecoders.github.io',
   sitemap: { hostname: 'https://decrystal.github.io' },
 });
